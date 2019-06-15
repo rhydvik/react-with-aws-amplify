ab3aefa2f77ae4ab38bc6b483856a1474f067795
Vikram Thakur <rhydvik@gmail.com>
2019-06-15 23:29:49 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Authenticator, AwsSignIn, Greetings } from 'aws-amplify-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Routes from './Routes';
+
+import authenticatorConfig from './config/authenticatorConfig';
+
+console.log(authenticatorConfig, 'authenticatorConfig');
+console.log(process, process.env);
+
+const App = () => (
+  <Authenticator amplifyConfig={authenticatorConfig}>
+    <Routes />
+  </Authenticator>
+);
 
 export default App;
---
+++ src/config/authenticatorConfig.js
@@ -0,0 +1,6 @@
+export default {
+  aws_cognito_identity_pool_id: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+  aws_user_pools_id: process.env.REACT_APP_USER_POOL_ID,
+  aws_cognito_region: process.env.REACT_APP_COGNITO_REFION,
+  aws_user_pools_web_client_id: process.env.REACT_APP_WEB_CLIENT_ID,
+};
