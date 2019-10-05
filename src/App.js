6edf7149476320e5ba88342421dcda3a8ed0938b
Vikram Thakur <rhydvik@gmail.com>
2019-10-05 12:46:00 +0530

    added redux and actions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Authenticator } from 'aws-amplify-react';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import Routes from './Routes';
 
 import authenticatorConfig from './config/authenticatorConfig';
 
-// TODO to check if values are coming or not
-console.log(authenticatorConfig, 'authenticatorConfig'); // eslint-disable-line
+import configureStore from './redux/configureStore';
+
+// // TODO to check if values are coming or not
+// console.log(authenticatorConfig, 'authenticatorConfig'); // eslint-disable-line
+
+const store = configureStore();
 
 const App = () => (
-  <Authenticator amplifyConfig={authenticatorConfig}>
-    <Routes />
-  </Authenticator>
+  <ReduxProvider store={store}>
+    <Authenticator amplifyConfig={authenticatorConfig}>
+      <Routes />
+    </Authenticator>
+  </ReduxProvider>
 );
 
 export default App;
--- src/views/profile/Profile.js
+++ src/views/profile/Profile.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
-const Profile = () => {
+import { connect } from 'react-redux';
+import { updateProfile } from '../../redux/modules/user';
+
+type Props = {
+  user: Object,
+  dispatch: Function,
+};
+
+const Profile = ({ user, dispatch }: Props) => {
+  const [name, setName] = useState('');
+
+  const updateRedux = () => {
+    dispatch(updateProfile({ name }));
+  };
+
   return (
     <>
-      <h1>profile</h1>
-      Go to <Link to="/dashboard"> Dashboard </Link>
+      <input onChange={event => setName(event.target.value)} /> <br />
+      <button onClick={updateRedux}>Update Redux</button>
+      <h3>Type and submit to see value here: {user && user.name}</h3>
     </>
   );
 };
 
-export default Profile;
+const mapStateToProps = ({ user }) => ({
+  user: user,
+});
+
+export default connect(mapStateToProps)(Profile);
